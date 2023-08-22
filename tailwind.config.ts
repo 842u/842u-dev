299b9e81b1dd77a2c42a54291c75dde59d80d844
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-22 12:02:44 +0200

    feat: choose basic color palette and font style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,14 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      background: '#222222',
+      text: '#faebd7',
+      accent: '#ffed50',
+      succes: '#68ff77',
+      error: '#ff5a5a',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
