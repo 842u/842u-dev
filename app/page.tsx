f21dd8ec25a7c2d4a81006861161db0f18e927aa
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-22 15:22:44 +0200

    feat: add basic responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="my-9 text-9xl font-bold">
-        <span className="after:mx-4 after:border-2 after:content-['']">
+      <h1 className="text-9xl font-bold">
+        <span className="relative block after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-[90%] after:translate-x-[-50%] after:bg-text md:inline md:after:static md:after:bottom-auto md:after:left-auto md:after:mx-4 md:after:inline md:after:h-auto md:after:translate-x-0 md:after:border-2">
           kamil
         </span>
-        <span>bażanow</span>
+        <span className="break-all">bażanow</span>
       </h1>
     </main>
   );
