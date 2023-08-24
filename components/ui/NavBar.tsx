945f1144f26f378facba23856bf9c32015729d79
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-24 13:29:58 +0200

    refactor: make Link take 100% space of li

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -1,5 +1,14 @@
 import Link from 'next/link';
 
+function SelectionIndicator() {
+  return (
+    <div
+      aria-hidden
+      className="pointer-events-none relative mr-4 inline-block h-10 w-10 overflow-hidden border-2 before:absolute before:right-2/4 before:h-full before:w-full before:rotate-45 before:border-r-4 before:border-t-4 after:absolute after:box-border after:h-full after:w-full after:border-8 after:border-background"
+    />
+  );
+}
+
 function NavBarItem({
   children,
   href,
@@ -9,7 +18,10 @@ function NavBarItem({
 }) {
   return (
     <li className="relative text-6xl before:my-2 before:block before:h-0.5 before:w-full before:bg-text after:absolute after:my-2 after:block after:h-0.5 after:w-full after:bg-text">
-      <Link href={href}>{children}</Link>
+      <Link className="block h-full w-full" href={href}>
+        <SelectionIndicator />
+        {children}
+      </Link>
     </li>
   );
 }
@@ -17,7 +29,7 @@ function NavBarItem({
 export function NavBar() {
   return (
     <header>
-      <nav className="fixed z-50 flex h-screen w-screen flex-col justify-center border-2 bg-background">
+      <nav className="fixed z-50 flex h-screen w-screen flex-col justify-center bg-background">
         <ul className="p-4">
           <NavBarItem href="/">home</NavBarItem>
           <NavBarItem href="/about">about</NavBarItem>
