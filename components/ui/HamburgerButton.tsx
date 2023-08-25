42c6d74cc8e8ec5b83274ffa7b0e2572d4920c5a
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-25 13:19:02 +0200

    feat: add hamburger button active state animations

--- components/ui/HamburgerButton.tsx
+++ components/ui/HamburgerButton.tsx
@@ -9,11 +9,23 @@ export function HamburgerButton({
 }: HamburgerButtonProps) {
   return (
     <button
-      aria-label="Menu"
+      aria-label="menu"
       className={`${className} ${
-        isActive ? 'bg-light' : 'bg-dark'
-      } fixed right-0 top-0 aspect-square w-20 border-2 border-dark`}
+        isActive ? 'asd' : 'asd'
+      } group fixed right-0 top-0 aspect-square w-20 border-dark p-3`}
       type="button"
-    />
+    >
+      <div className="flex aspect-square w-full flex-col items-center justify-between overflow-hidden">
+        <div className="h-1 w-full bg-dark transition-opacity group-hover:opacity-0" />
+
+        <div className="relative w-full">
+          <div className="absolute h-1 w-full origin-center bg-dark transition-transform group-hover:rotate-45 group-hover:scale-[2]" />
+
+          <div className="absolute h-1 w-full origin-center bg-dark transition-transform group-hover:rotate-[-45deg] group-hover:scale-[2]" />
+        </div>
+
+        <div className="h-1 w-full bg-dark transition-opacity group-hover:opacity-0" />
+      </div>
+    </button>
   );
 }
