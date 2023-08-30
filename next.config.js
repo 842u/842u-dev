5b2c37a9be433dacf97e4db060d4a2c24f618b74
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-30 16:24:26 +0200

    feat: add basic CSP in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,61 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+    default-src 'self';
+    connect-src 'self';
+    script-src 'self' 'unsafe-inline';
+    style-src 'self';
+    img-src 'self';
+    font-src 'self';
+  `;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value:
+      'camera=(), microphone=(), geolocation=() , accelerometer=(), gyroscope=(), magnetometer=(), payment=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {};
+  }
+
+  return {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: securityHeaders,
+        },
+      ];
+    },
+  };
+};
