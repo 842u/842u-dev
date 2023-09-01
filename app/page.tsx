de3cc98dd4e2c45c15849b024417a4f9eeaaaa76
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-09-01 10:43:17 +0200

    feat: add HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import { HeroSection } from '@/components/sections/HeroSection';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="divide-x-2 text-5xl">
-        <span className="p-2">kamil</span>
-        <span className="p-2">bażanow</span>
-      </h1>
+    <main>
+      <HeroSection />
     </main>
   );
 }
