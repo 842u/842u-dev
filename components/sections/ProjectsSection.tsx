1418aa3e56ad62bf0fcab43272d47a49cec7c7aa
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-09-06 10:37:17 +0200

    feat: set ProjectsSection paddings

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -1,3 +1,7 @@
 export function ProjectsSection() {
-  return <section className="h-screen">HELLO</section>;
+  return (
+    <section className="h-screen border-2 border-light px-4 md:px-16 lg:px-36">
+      HELLO
+    </section>
+  );
 }
