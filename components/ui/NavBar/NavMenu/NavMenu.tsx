c55d9288c05c18016456315122f63a525c4a1453
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-09-08 12:41:00 +0200

    refactor: change NavMenu styling

--- components/ui/NavBar/NavMenu/NavMenu.tsx
+++ components/ui/NavBar/NavMenu/NavMenu.tsx
@@ -10,7 +10,7 @@ export function NavMenu({ items, isActive = false }: NavMenuProps) {
     <menu
       className={`${
         isActive ? 'operational' : ''
-      } absolute left-full top-0 flex h-screen w-screen flex-col justify-center px-6 text-6xl transition-all operational:left-0 operational:bg-light operational:dark:bg-dark md:static md:h-3/4 md:w-auto md:translate-x-0 md:flex-row md:divide-x-2 md:px-0 md:text-5xl`}
+      } absolute left-full top-0 flex h-screen w-screen flex-col justify-center px-6 text-6xl transition-all operational:left-0 operational:bg-light operational:dark:bg-dark md:static md:h-3/4 md:w-auto md:translate-x-0 md:flex-row md:divide-x-2 md:px-0 md:text-4xl`}
     >
       {items?.map((item) => (
         <NavMenuItem key={item.name} className="md:px-5" href={item.href}>
--- components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
+++ components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
@@ -16,9 +16,9 @@ type NavMenuItemProps = {
 export function NavMenuItem({ children, className, href }: NavMenuItemProps) {
   return (
     <li
-      className={`${className} relative text-dark-lighter before:top-0 before:block before:h-0.5 before:w-full before:bg-dark-lighter after:absolute after:block after:h-0.5 after:w-full after:bg-dark-lighter dark:text-light-darker dark:before:bg-light-darker dark:after:bg-light-darker md:before:hidden md:after:hidden`}
+      className={`${className} relative text-dark-lighter before:top-0 before:block before:h-0.5 before:w-full before:bg-dark-lighter after:absolute after:block after:h-0.5 after:w-full after:bg-dark-lighter dark:text-light-darker dark:before:bg-light-darker dark:after:bg-light-darker md:my-2 md:before:hidden md:after:hidden`}
     >
-      <Link className="my-4 block md:my-0 md:h-full" href={href}>
+      <Link className="my-3 block md:my-0 md:h-full" href={href}>
         <div className="flex h-full w-full items-center gap-6">
           <SelectionArrow className="md:hidden" />
           {children}
