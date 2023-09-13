a38d6e3b309bf5f989d1853cb112b7023d322f50
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-09-13 15:12:08 +0200

    feat: add createSlug() tests

--- utils/createSlug.ts
+++ utils/createSlug.ts
@@ -1,11 +1,11 @@
 export function createSlug(text: string) {
-  const parts = text
+  const segments = text
     .trim()
     .replace(/[^A-Za-z0-9\s]/g, ' ')
     .split(' ')
     .filter((part) => !!part);
 
-  const lowerCaseParts = parts.map((part) => part.toLocaleLowerCase());
+  const lowerCaseParts = segments.map((part) => part.toLocaleLowerCase());
 
   const slug = lowerCaseParts.join('-');
 
