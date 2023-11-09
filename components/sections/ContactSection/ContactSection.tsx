5ac411f3cc19c8b294bd1f5df4e96c51f49c2610
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-09 14:56:21 +0100

    feat: create ContactForm

--- components/sections/ContactSection/ContactSection.tsx
+++ components/sections/ContactSection/ContactSection.tsx
@@ -1,131 +1,15 @@
-/* eslint react/jsx-props-no-spreading: 0, no-nested-ternary: 0  */
-
-'use client';
-
-import { useEffect } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-
-import { Button } from '@/components/ui/Button/Button';
+import { ContactForm } from '@/components/ui/ContactForm/ContactForm';
 
 import { Section } from '../Section';
 
-type FormValues = {
-  name: string;
-  email: string;
-  message: string;
-};
-
 export function ContactSection() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitted, isSubmitSuccessful, isValid },
-  } = useForm<FormValues>({
-    defaultValues: {
-      name: '',
-      email: '',
-      message: '',
-    },
-  });
-
-  useEffect(() => {
-    reset();
-  }, [isSubmitSuccessful]);
-
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    // eslint-disable-next-line
-    console.log(data);
-  };
-
   return (
     <Section title="contact">
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className={`mt-6 border-b-2 bg-light text-2xl placeholder:text-2xl placeholder:text-dark-lighter dark:bg-dark ${
-            isSubmitted
-              ? errors.name?.message
-                ? 'border-error dark:border-error'
-                : 'border-success dark:border-success'
-              : 'border-dark dark:border-light'
-          }`}
-          {...register('name', {
-            required: 'This field is required.',
-            minLength: {
-              value: 3,
-              message: 'Minimum length is 3.',
-            },
-            maxLength: {
-              value: 50,
-              message: 'Maximum length is 50.',
-            },
-          })}
-          placeholder="Enter your name ..."
-          type="text"
-        />
-        <p className="whitespace-pre-wrap text-right">
-          {errors.name?.message || ' '}
-        </p>
-
-        <input
-          className={`mt-6 border-b-2 bg-light text-2xl placeholder:text-2xl placeholder:text-dark-lighter dark:bg-dark ${
-            isSubmitted
-              ? errors.email?.message
-                ? 'border-error dark:border-error'
-                : 'border-success dark:border-success'
-              : 'border-dark dark:border-light'
-          }`}
-          {...register('email', {
-            required: 'This field is required.',
-            minLength: {
-              value: 3,
-              message: 'Minimum length is 3.',
-            },
-            maxLength: {
-              value: 254,
-              message: 'Maximum length is 254.',
-            },
-            pattern: {
-              value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i,
-              message: 'Enter valid e-mail adress.',
-            },
-          })}
-          placeholder="Enter your e-mail ..."
-          type="email"
-        />
-        <p className="whitespace-pre-wrap text-right">
-          {errors.email?.message || ' '}
-        </p>
-
-        <textarea
-          className={`mt-6 border-b-2 bg-light text-2xl placeholder:text-2xl placeholder:text-dark-lighter dark:bg-dark ${
-            isSubmitted
-              ? errors.message?.message
-                ? 'border-error dark:border-error'
-                : 'border-success dark:border-success'
-              : 'border-dark dark:border-light'
-          }`}
-          {...register('message', {
-            required: 'This field is required.',
-            minLength: {
-              value: 10,
-              message: 'Minimum length is 10.',
-            },
-            maxLength: {
-              value: 3000,
-              message: 'Maximum length is 3000.',
-            },
-          })}
-          placeholder="Enter your message ..."
-        />
-        <p className="whitespace-pre-wrap text-right">
-          {errors.message?.message || ' '}
-        </p>
-
-        <Button disabled={isSubmitted && !isValid} type="submit">
-          Send
-        </Button>
-      </form>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
+        excepturi.
+      </p>
+      <ContactForm />
     </Section>
   );
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -7,13 +7,14 @@ import { SelectionArrow } from '@/components/decorative/SelectionArrow';
 type ButtonProps = {
   disabled?: boolean;
   type: 'button' | 'submit' | 'reset';
+  className?: string;
   children: React.ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ children, type, disabled }: ButtonProps) {
+export function Button({ children, type, disabled, className }: ButtonProps) {
   return (
     <button
-      className="text-3xl before:block before:h-0.5 before:w-full before:bg-dark after:block after:h-0.5 after:w-full after:bg-dark disabled:text-light-darker disabled:before:bg-light-darker disabled:after:bg-light-darker dark:before:bg-light dark:after:bg-light disabled:dark:text-dark-lighter disabled:dark:before:bg-dark-lighter disabled:dark:after:bg-dark-lighter md:w-64 md:min-w-fit md:text-4xl"
+      className={`text-3xl before:block before:h-0.5 before:w-full before:bg-dark after:block after:h-0.5 after:w-full after:bg-dark disabled:text-light-darker disabled:before:bg-light-darker disabled:after:bg-light-darker dark:before:bg-light dark:after:bg-light disabled:dark:text-dark-lighter disabled:dark:before:bg-dark-lighter disabled:dark:after:bg-dark-lighter md:w-64 md:min-w-fit md:text-4xl ${className}`}
       disabled={disabled}
       type={type}
     >
