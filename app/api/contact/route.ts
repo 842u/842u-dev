24cd1d9e5b905893ff0efd78efaa174c6bc63b17
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-13 13:49:46 +0100

    feat: add server side form validation

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,33 +1,73 @@
+/* eslint  simple-import-sort/imports: 0 */
+
 import sendgridMail from '@sendgrid/mail';
 import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
+import {
+  InputValidationError,
+  emailInputValidationRules,
+  messageInputValidationRules,
+  nameInputValidationRules,
+  validateInput,
+} from '@/utils/validation';
+import { ContactFormValues, FormInputsIdentifiers } from '@/types';
 
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
-export async function GET() {
+export async function POST(request: Request) {
+  const { name, email, message } = (await request.json()) as ContactFormValues;
+
   const responseEmail = {
     to: 'kamil.bazanow@gmail.com',
     from: 'contact@842u.dev',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
+    subject: '842u.dev: Someone wants to contact with you!',
     html: `<h1>Hello Kamil. You have got a message from contact form. Here it is:</h1>
-    <p>Name</p>
-    <p>Email</p>
-    <p>Message</p>`,
+    <p>Name: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>${message}</p>`,
   };
+
+  try {
+    validateInput(FormInputsIdentifiers.Name, name, nameInputValidationRules);
+    validateInput(
+      FormInputsIdentifiers.Email,
+      email,
+      emailInputValidationRules,
+    );
+    validateInput(
+      FormInputsIdentifiers.Message,
+      message,
+      messageInputValidationRules,
+    );
+  } catch (error) {
+    if (error instanceof InputValidationError)
+      return NextResponse.json(
+        {
+          message: `Server validation error: ${error.message} Try again.`,
+          inputIdentifier: error.inputIdentifier,
+        },
+        { status: 400 },
+      );
+  }
 
   try {
     await sendgridMail.send(responseEmail);
 
-    return NextResponse.json({
-      message: 'Got your message. I will contact with you as soon as possible.',
-      status: 200,
-    });
+    return NextResponse.json(
+      {
+        message:
+          'Got your message. I will contact with you as soon as possible.',
+      },
+      { status: 200 },
+    );
   } catch (error) {
-    return NextResponse.json({
-      message: `Something went wrong with mailing service: ${error}`,
-      status: 400,
-    });
+    // eslint-disable-next-line
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: `Something went wrong with mailing service. Please try again.`,
+      },
+      { status: 400 },
+    );
   }
 }
