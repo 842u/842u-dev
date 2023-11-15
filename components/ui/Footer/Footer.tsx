62a12410e2be91c5522a233058887fcd09f6c32e
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-15 19:47:51 +0100

    refactor: style adjustments

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -5,8 +5,8 @@ import { MailIcon } from '@/components/icons/MailIcon';
 export function Footer() {
   return (
     <footer>
-      <address className="flex flex-col items-center gap-10 p-5 not-italic lg:flex-row lg:justify-between">
-        <div className="flex h-10 w-full justify-evenly lg:h-7 lg:w-1/5">
+      <address className="flex flex-col items-center gap-10 p-5 not-italic lg:flex-row lg:justify-between lg:px-10">
+        <div className="flex h-10 w-full justify-evenly lg:h-7 lg:w-1/6 lg:justify-between">
           <GitHubIcon className="h-full" href="https://github.com/842u" />
           <div aria-hidden className="h-full w-0.5 bg-dark dark:bg-light" />
           <LinkedinIcon
