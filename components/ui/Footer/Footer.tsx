6fd24881860d3d6a5b475832d255eb131f18627e
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-15 18:48:13 +0100

    feat: add responsive styling for Footer

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -4,15 +4,18 @@ import { MailIcon } from '@/components/icons/MailIcon';
 
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center gap-10">
-      <address className="flex h-10 w-full justify-evenly">
-        <GitHubIcon href="https://github.com/842u" />
+    <footer className="flex flex-col items-center gap-10 p-5 lg:flex-row lg:justify-between">
+      <address className="flex h-10 w-full justify-evenly lg:h-7 lg:w-1/5">
+        <GitHubIcon className="h-full" href="https://github.com/842u" />
         <div className="h-full w-0.5 bg-dark dark:bg-light" />
-        <LinkedinIcon href="https://www.linkedin.com/in/kamil-ba%C5%BCanow-025837234" />
+        <LinkedinIcon
+          className="h-full"
+          href="https://www.linkedin.com/in/kamil-ba%C5%BCanow-025837234"
+        />
         <div className="h-full w-0.5 bg-dark dark:bg-light" />
-        <MailIcon href="mailto:contact@842u.dev" />
+        <MailIcon className="h-full" href="mailto:contact@842u.dev" />
       </address>
-      <div className="m-5 divide-x-2 text-xl">
+      <div className="divide-x-2 text-xl lg:m-0 lg:text-base">
         <span className="pr-2">Kamil</span>
         <span className="pl-2">Bażanow</span>
       </div>
