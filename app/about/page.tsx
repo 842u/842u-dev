b8270a86aca937eea0e8f34e0e8dd40e9aeb6237
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-16 16:21:14 +0100

    feat: add AboutPage barebones

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import { Section } from '@/components/sections/Section';
+import { SectionParagraph } from '@/components/ui/SectionParagraph';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <Section className="flex items-center">
+        <SectionParagraph>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At porro
+          aperiam adipisci provident est nulla, laudantium cumque et dolore
+          cupiditate amet dignissimos enim rem maxime!
+        </SectionParagraph>
+      </Section>
+    </main>
+  );
+}
