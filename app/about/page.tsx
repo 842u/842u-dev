3a5840b086bc8b546da5662cb462fab92bef2389
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-16 16:38:51 +0100

    feat: add svgPaths data

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
+import { CMR2SVG } from '@/components/decorative/CMR2SVG';
 import { Section } from '@/components/sections/Section';
 import { SectionParagraph } from '@/components/ui/SectionParagraph';
+import { alienatedSVGPath } from '@/data/svgPaths';
 
 export default function AboutPage() {
   return (
@@ -10,6 +12,16 @@ export default function AboutPage() {
           aperiam adipisci provident est nulla, laudantium cumque et dolore
           cupiditate amet dignissimos enim rem maxime!
         </SectionParagraph>
+        <CMR2SVG
+          animationSpeed={10}
+          characterRotation="0"
+          characterSpacing={0.08}
+          className="w-1/3 fill-light-darker text-[5px] dark:text-dark-lighter"
+          pathShape={alienatedSVGPath}
+          text=" kamil bażanow "
+          textRepetition={9}
+          viewBox="0 0 200 200"
+        />
       </Section>
     </main>
   );
