25e231e189366a2fa2190236a4da75fc1c644e07
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-11-21 18:07:49 +0100

    test: performance test of my CMR2SVG

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,60 +1,30 @@
 'use client';
 
-import { useState } from 'react';
-
 import { CMR2SVG } from '@/components/decorative/CMR2SVG';
 import { Section } from '@/components/sections/Section';
-import { HorizontalMenu } from '@/components/ui/HorizontalMenu/HorizontalMenu';
-import { ProjectCard } from '@/components/ui/ProjectCard/ProjectCard';
-import { allProjects } from '@/data/projects/allProjects';
 import { bazuDevSVGPath } from '@/data/svgPaths';
-import { Project } from '@/types';
 
-type AllProjectsSectionProps = {
-  projects: Project[];
-};
-
-function AllProjectsSection({ projects }: AllProjectsSectionProps) {
-  const [currentProject, setCurrentProject] = useState(projects[0]);
-
-  const menuItemClickHandler = (event: React.MouseEvent) => {
-    const clickedProject = projects.find(
-      (project) =>
-        project.name.toLowerCase() ===
-        (event.target as HTMLButtonElement).innerText,
-    );
-
-    setCurrentProject(clickedProject || projects[0]);
-  };
-
+function AllProjectsSection() {
   return (
-    <div className="min-h-screen">
-      <Section ariaLabel="all projects" title="Projects">
-        <CMR2SVG
-          animationDuration={30}
-          characterRotation="0"
-          characterSpacing={0.08}
-          className="fill-light-darker text-[3px] dark:text-dark-lighter md:block"
-          pathShape={bazuDevSVGPath}
-          text="842u.dev "
-          textRepetition={42}
-          viewBox="0 0 292 57"
-        />
-        <HorizontalMenu className="my-10" onClick={menuItemClickHandler}>
-          {projects.map((project) => project.name)}
-        </HorizontalMenu>
-      </Section>
-      <Section>
-        <ProjectCard project={currentProject} />
-      </Section>
-    </div>
+    <Section ariaLabel="all projects" title="Projects">
+      <CMR2SVG
+        animationDuration={30}
+        characterRotation="0"
+        characterSpacing={0.08}
+        className="fill-light-darker text-[3px] dark:text-dark-lighter md:block"
+        pathShape={bazuDevSVGPath}
+        text="842u.dev "
+        textRepetition={42}
+        viewBox="0 0 292 57"
+      />
+    </Section>
   );
 }
 
 export default function ProjectsPage() {
   return (
     <main>
-      <AllProjectsSection projects={allProjects} />
+      <AllProjectsSection />
     </main>
   );
 }
