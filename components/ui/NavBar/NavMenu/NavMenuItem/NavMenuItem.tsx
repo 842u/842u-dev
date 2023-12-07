dcddd0f243a81e0b7a1366bea0f95ca140d3e592
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-12-07 17:53:31 +0100

    feat: add styling for active NavMenuItems

--- components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
+++ components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { SelectionArrow } from '@/components/decorative/SelectionArrow';
 
@@ -13,9 +16,19 @@ type NavMenuItemProps = {
 };
 
 export function NavMenuItem({ children, href }: NavMenuItemProps) {
+  const pathname = usePathname();
+
+  const isActive =
+    pathname === href || (pathname.includes(href) && href !== '/');
+
   return (
     <li className="relative text-dark-lighter before:top-0 before:block before:h-0.5 before:w-full before:bg-dark-lighter after:absolute after:block after:h-0.5 after:w-full after:bg-dark-lighter dark:text-light-darker dark:before:bg-light-darker dark:after:bg-light-darker md:my-2 md:px-4 md:before:hidden md:after:hidden">
-      <Link className="my-3 block md:my-0 md:h-full" href={href}>
+      <Link
+        className={`${
+          isActive ? 'operational' : ''
+        } my-3 block operational:text-dark operational:dark:text-light md:my-0 md:h-full`}
+        href={href}
+      >
         <div className="flex h-full w-full items-center gap-6">
           <SelectionArrow className="md:hidden" />
           {children}
