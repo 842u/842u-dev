693e21cabf7eb3d376a83f8e5154caacef80f1fe
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-12-07 12:17:44 +0100

    refactor: NavBar style adjustments

--- components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
+++ components/ui/NavBar/NavMenu/NavMenuItem/NavMenuItem.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { twMerge } from 'tailwind-merge';
 
 import { SelectionArrow } from '@/components/decorative/SelectionArrow';
 
@@ -11,17 +10,11 @@ export type NavItem = {
 type NavMenuItemProps = {
   children: React.ReactNode;
   href: string;
-  className?: string;
 };
 
-export function NavMenuItem({ children, className, href }: NavMenuItemProps) {
+export function NavMenuItem({ children, href }: NavMenuItemProps) {
   return (
-    <li
-      className={twMerge(
-        `relative text-dark-lighter before:top-0 before:block before:h-0.5 before:w-full before:bg-dark-lighter after:absolute after:block after:h-0.5 after:w-full after:bg-dark-lighter dark:text-light-darker dark:before:bg-light-darker dark:after:bg-light-darker md:my-2 md:before:hidden md:after:hidden`,
-        className,
-      )}
-    >
+    <li className="relative text-dark-lighter before:top-0 before:block before:h-0.5 before:w-full before:bg-dark-lighter after:absolute after:block after:h-0.5 after:w-full after:bg-dark-lighter dark:text-light-darker dark:before:bg-light-darker dark:after:bg-light-darker md:my-2 md:px-4 md:before:hidden md:after:hidden">
       <Link className="my-3 block md:my-0 md:h-full" href={href}>
         <div className="flex h-full w-full items-center gap-6">
           <SelectionArrow className="md:hidden" />
