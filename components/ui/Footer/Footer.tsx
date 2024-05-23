78a1540b4ee07bfe769c25aa4a808f67a85bcb67
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-05-23 11:42:29 +0200

    style(Footer): add on hover animation

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -7,14 +7,20 @@ export function Footer() {
     <footer>
       <address className="flex flex-col items-center gap-10 p-5 not-italic lg:flex-row lg:justify-between lg:px-10">
         <div className="flex h-10 w-full justify-evenly md:w-1/2 lg:h-7 lg:w-1/6 lg:justify-between">
-          <GitHubIcon className="h-full" href="https://github.com/842u" />
+          <GitHubIcon
+            className="h-full hover:animate-blink"
+            href="https://github.com/842u"
+          />
           <div aria-hidden className="h-full w-0.5 bg-dark dark:bg-light" />
           <LinkedinIcon
-            className="h-full"
+            className="h-full hover:animate-blink"
             href="https://www.linkedin.com/in/kamil-ba%C5%BCanow-025837234"
           />
           <div aria-hidden className="h-full w-0.5 bg-dark dark:bg-light" />
-          <MailIcon className="h-full" href="mailto:contact@842u.dev" />
+          <MailIcon
+            className="h-full hover:animate-blink"
+            href="mailto:contact@842u.dev"
+          />
         </div>
         <p className="divide-x-2 text-xl lg:m-0 lg:text-base">
           <span className="pr-2">kamil</span>
