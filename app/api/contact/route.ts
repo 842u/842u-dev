49ab36eae0c7fb821ea9c899f3116180c8b65a8c
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-02-20 12:24:03 +0100

    chore(@sendgrid/mail): remove dependency

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,4 +1,3 @@
-import sendgridMail from '@sendgrid/mail';
 import { NextResponse } from 'next/server';
 
 import { ContactFormValues, FormInputsIdentifiers } from '@/types';
@@ -10,20 +9,20 @@ import {
   validateInput,
 } from '@/utils/validation';
 
-sendgridMail.setApiKey(process.env.SENDGRID_API_KEY!);
+// sendgridMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 export async function POST(request: Request) {
   const { name, email, message } = (await request.json()) as ContactFormValues;
 
-  const responseEmail = {
-    to: 'kamil.bazanow@gmail.com',
-    from: 'contact@842u.dev',
-    subject: '842u.dev: Someone wants to contact with you!',
-    html: `<h1>Hello Kamil. You have got a message from contact form. Here it is:</h1>
-    <p>Name: ${name}</p>
-    <p>Email: ${email}</p>
-    <p>${message}</p>`,
-  };
+  // const responseEmail = {
+  //   to: 'kamil.bazanow@gmail.com',
+  //   from: 'contact@842u.dev',
+  //   subject: '842u.dev: Someone wants to contact with you!',
+  //   html: `<h1>Hello Kamil. You have got a message from contact form. Here it is:</h1>
+  //   <p>Name: ${name}</p>
+  //   <p>Email: ${email}</p>
+  //   <p>${message}</p>`,
+  // };
 
   try {
     validateInput(FormInputsIdentifiers.Name, name, nameInputValidationRules);
@@ -49,7 +48,7 @@ export async function POST(request: Request) {
   }
 
   try {
-    await sendgridMail.send(responseEmail);
+    // await sendgridMail.send(responseEmail);
 
     return NextResponse.json(
       {
